5c7d9ef9b1a8298569cd8f4ec1c64d5fb57b867d
RSMarian <marianstefan1499@gmail.com>
2022-06-28 17:24:13 +0300

    added buttons functionality

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const slides = document.querySelectorAll('.slide')
-const nextBtn = document.querySelectorAll('.nextBtn')
-const prevBtn = document.querySelectorAll('.prevBtn')
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelectorAll(".nextBtn");
+const prevBtn = document.querySelectorAll(".prevBtn");
 
-slides.forEach(function(slide,index){
-    slide.style.left = `${index * 100}%`
-})
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
